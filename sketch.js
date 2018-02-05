b2f2478561029908fad74569e47d07c115fcd9b1
Pasquale Morelli <pasquale.morelli@mail.polimi.it>
2018-02-06 00:15:44 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,28 +1,20 @@
 function setup() {
-  createCanvas(500,500);
-  background(40);
+  createCanvas(500, 500);
+  background(0);
   angleMode(DEGREES);
-  strokeWeight(0.2)
+
 }
 
 function draw() {
-  translate(width/2,height/2)
-  
-  strokeWeight(0.2)
-  stroke(color('#fff22d'));
-  line(width*cos(-frameCount*6), width*sin(-frameCount*3),width/2,0);
-  
-  strokeWeight(0.5)
-  stroke(color('#ca0088'));
-  line(width/2*cos(-frameCount*2), width/2*sin(-frameCount*2),-width,0);
-  
-  translate(-width/2,-height/2)
-  
-  strokeWeight(0.2)
-  stroke(lerpColor(color('#ea0043'),color('#0fefca'), frameCount/180));
-  line(0*cos(-frameCount*2), width*sin(-frameCount*2),height,0);
-  
-  if (frameCount >= 180) {
+  translate(width/2,height/2);
+  rotate(frameCount*6);
+  noFill();
+
+  stroke(lerpColor(color('#330a5b'), color('#f90089'), frameCount/450));
+  line(125*sin(frameCount),0,125*sin(frameCount),125)
+
+
+  if (frameCount == 450) {
     noLoop();
   }
 }
